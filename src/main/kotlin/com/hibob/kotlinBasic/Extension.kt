1210a02d5df1eaa455845046515e39eaa4e01211
Shahar Shwartz <shahar.shwartz@hibob.io>
2024-09-03 10:56:39 +0300

    Finish extension assignment

--- src/main/kotlin/com/hibob/kotlinBasic/Extension.kt
+++ src/main/kotlin/com/hibob/kotlinBasic/Extension.kt
@@ -1,5 +1,35 @@
 package com.hibob.kotlinBasic
 
-class Extension {
+
+
+fun List<Int>.sum() : Int {
+
+        var result = 0
+        map {
+            result += it
+        }
+
+        return result
+    }
+
+infix fun Number.toPowerOf(exponent : Number) : Double {
+
+    return Math.pow(this.toDouble(), exponent.toDouble())
+}
+
+
+
+
+fun main(args : Array<String>) {
+
+    val sum = listOf(1,2,3).sum()
+    println(sum)
+
+    val number = 2
+    var result = number.toPowerOf(2)
+    println(result)
+
+    result = number.toPowerOf(4)
+    println(result)
 
 }
