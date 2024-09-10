d841dbd760e4a39c20d83bc636dbf494c3edc898
Shahar Shwartz <shahar.shwartz@hibob.io>
2024-09-10 10:08:04 +0300

    fixed after the comments

--- src/main/resources/db/migration/V202409081400__create_pet_table_and_index.sql
+++ src/main/resources/db/migration/V202409081400__create_pet_table_and_index.sql
@@ -3,10 +3,9 @@ create table pets
     id              UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name            VARCHAR(255),
     type            VARCHAR(255),
-    data_of_arrival DATE,
+    data_of_arrival DATE DEFAULT CURRENT_DATE,
     company_id      UUID
 );
 
-create index idx_pets_company_id
-    on pets (company_id);
+create index idx_pets_company_id on pets (company_id);
 
--- src/main/resources/db/migration/V202409081500__create_owner_table_and_index.sql
+++ src/main/resources/db/migration/V202409081500__create_owner_table_and_index.sql
@@ -3,11 +3,10 @@ create table owner
     id          UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name        VARCHAR(255),
     company_id  UUID,
-    employee_id UUID
+    employee_id VARCHAR(255)
 );
 
-create index idx_owner_company_id_employee_id
-    on owner (company_id, employee_id);
+create index idx_owner_company_id_employee_id on owner (company_id, employee_id);
 
 
 /*
@@ -17,9 +16,7 @@ create index idx_owner_company_id_employee_id
     insert into pets(name, type, data_of_arrival, company_id)
     values('Tom', 'Cat', '2024-09-08', 'f7b3b3b3-7b3b-4b3b-8b3b-3b3b3b3b3b3b');
 
-    select * from pets where type = 'Dog';
-
+select distinct * from pets where type = 'Dog';
     delete from pets where id = '8b3fe77a-d799-4684-ad3a-7794e4e25bb5';
 
-    select * from pets where data_of_arrival < '2023-09-08';
- */
+select * from pets where data_of_arrival < CURRENT_DATE - INTERVAL '1 year'; */
