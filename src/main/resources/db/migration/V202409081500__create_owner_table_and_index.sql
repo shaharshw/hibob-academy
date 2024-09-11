918af062ac091e275dd27555624af048e13c6584
Shahar Shwartz <shahar.shwartz@hibob.io>
2024-09-11 10:35:19 +0300

    Finish after the comments

--- src/main/resources/db/migration/V202409081500__create_owner_table_and_index.sql
+++ src/main/resources/db/migration/V202409081500__create_owner_table_and_index.sql
@@ -1,8 +1,8 @@
 create table owner
 (
     id          UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    name        VARCHAR(255),
-    company_id  UUID,
+    name        VARCHAR(255) NOT NULL,
+    company_id  BIGINT NOT NULL,
     employee_id VARCHAR(255)
 );
 
