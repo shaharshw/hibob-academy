b490a0d1aba869709619bc95a6fccbc9beb6ba06
Shahar Shwartz <shahar.shwartz@hibob.io>
2024-09-16 09:32:08 +0300

    Finish sql2 exercise

--- src/main/kotlin/com/hibob/academy/service/PetService.kt
+++ src/main/kotlin/com/hibob/academy/service/PetService.kt
@@ -36,4 +36,10 @@ class PetService(
 
         return ownerById.owner
     }
+
+    fun getAllPetsByOwnerId(ownerId: Long) : List<Pet> =
+        petDao.getAllPetsByOwnerId(ownerId)
+
+    fun getCountPetsByType() : Map<String, Int> =
+        petDao.getCountPetsByType()
 }
