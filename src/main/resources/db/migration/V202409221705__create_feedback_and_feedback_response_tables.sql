12c125b5f6ce782c490ae454cb5f0d88c8dd41b3
Shahar Shwartz <shahar.shwartz@hibob.io>
2024-09-22 17:07:54 +0300

    Create feedback and feedback_response tables

---
+++ src/main/resources/db/migration/V202409221705__create_feedback_and_feedback_response_tables.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS feedback (
+                id BIGSERIAL PRIMARY KEY,
+                company_id BIGINT NOT NULL,
+                sender_id BIGINT NOT NULL,
+                text TEXT NOT NULL,
+                is_anonymous BOOLEAN DEFAULT FALSE,
+                created_at DATE DEFAULT CURRENT_DATE,
+                status VARCHAR(50) DEFAULT 'Unreviewed'
+    );
+
+
+CREATE TABLE IF NOT EXISTS feedback_response (
+                id BIGSERIAL PRIMARY KEY,
+                company_id BIGINT NOT NULL,
+                feedback_id BIGINT NOT NULL,
+                reviewer_id BIGINT NOT NULL,
+                response_text TEXT NOT NULL,
+                created_at DATE DEFAULT CURRENT_DATE
+    );
+
+
