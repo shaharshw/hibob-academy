eaa28ec99cf112da504523807ecb4319183f8e3f
Shahar Shwartz <shahar.shwartz@hibob.io>
2024-09-24 15:00:35 +0300

    little fix in models.kt

--- src/main/kotlin/com/hibob/academy/employeeFeedback/model/models.kt
+++ src/main/kotlin/com/hibob/academy/employeeFeedback/model/models.kt
@@ -2,8 +2,6 @@ package com.hibob.academy.employeeFeedback.model
 
 import java.time.LocalDate
 
-// Enum
-
 enum class FeedbackStatus {
     UNREVIEWED,
     REVIEWED
@@ -21,7 +19,6 @@ enum class Role {
     HR,
     ADMIN
 }
-
 
 data class Feedback(
     val id: Long,
@@ -88,4 +85,3 @@ data class UpdateFeedbackResponseRequest(
     val responseText: String,
     val append: Boolean = false
 )
-
