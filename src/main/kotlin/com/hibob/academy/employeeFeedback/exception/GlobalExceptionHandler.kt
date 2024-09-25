3a178039da09f0ada37834f2d3a0559ab03b37d6
Shahar Shwartz <99726451+shaharshw@users.noreply.github.com>
2024-09-25 15:51:01 +0300

    Create a global exception handler (#60)
    * Create GlobalExceptionHandler
    
    * fix GlobalExceptionHandler
    
    * fixed GlobalExceptionHandler
    
    * UPDATE  GlobalExceptionHandler
    
    ---------
    
    Co-authored-by: Shahar Shwartz <shahar.shwartz@hibob.io>

---
+++ src/main/kotlin/com/hibob/academy/employeeFeedback/exception/GlobalExceptionHandler.kt
@@ -0,0 +1,21 @@
+package com.hibob.academy.employeeFeedback.exception
+
+import jakarta.ws.rs.core.Response
+import jakarta.ws.rs.ext.ExceptionMapper
+import jakarta.ws.rs.ext.Provider
+import org.springframework.http.HttpStatus
+import org.springframework.web.server.ResponseStatusException
+
+@Provider
+class IllegalArgumentExceptionMapper : ExceptionMapper<IllegalArgumentException> {
+    override fun toResponse(ex: IllegalArgumentException): Response {
+        return Response.status(Response.Status.BAD_REQUEST).entity(ex.message).build()
+    }
+}
+
+@Provider
+class ResponseStatusExceptionMapper : ExceptionMapper<ResponseStatusException> {
+    override fun toResponse(ex: ResponseStatusException): Response {
+        return Response.status(Response.Status.FORBIDDEN).entity(ex.reason).build()
+    }
+}
