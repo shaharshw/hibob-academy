c9f35f28260c1b105012a6640968e8deffb9fc4c
Shahar Shwartz <shahar.shwartz@hibob.io>
2024-09-25 18:04:56 +0300

    remove upper method from Filter

--- src/main/kotlin/com/hibob/academy/employeeFeedback/dao/filter/Filter.kt
+++ src/main/kotlin/com/hibob/academy/employeeFeedback/dao/filter/Filter.kt
@@ -5,7 +5,6 @@ import com.hibob.academy.employeeFeedback.dao.table.FeedbackTable
 import com.hibob.academy.employeeFeedback.model.Department
 import com.hibob.academy.employeeFeedback.model.FeedbackStatus
 import org.jooq.Condition
-import org.jooq.impl.DSL.upper
 import java.sql.Date
 import java.time.LocalDate
 
@@ -15,7 +14,7 @@ sealed class Filter<T> {
     data class DepartmentFilter(private val department: Department?) : Filter<EmployeeTable>() {
         override fun apply(table: EmployeeTable): Condition? {
             return department?.name?.let {
-                upper(table.department).eq(it.uppercase())
+                table.department.eq(it.uppercase())
             }
         }
     }
@@ -29,7 +28,7 @@ sealed class Filter<T> {
     data class StatusFilter(private val status: FeedbackStatus?) : Filter<FeedbackTable>() {
         override fun apply(table: FeedbackTable): Condition? {
             return status?.name?.let {
-                upper(table.status).eq(it.uppercase())
+                table.status.eq(it.uppercase())
             }
         }
     }
