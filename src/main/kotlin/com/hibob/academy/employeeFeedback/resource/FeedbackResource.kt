b4bcb04510b0e9ddb9067252331845748fa4f59c
Shahar Shwartz <99726451+shaharshw@users.noreply.github.com>
2024-09-25 16:09:00 +0300

    Finish feedback resource (#62)
    * Fixed the bug when I wanted to filter the feedback with the department.
    
    * Fixed the bug when I wanted to filter the feedback with the status.
    
    * Finish FeedbackResource.
    
    * remove uppercase.
    
    * Remove empty lines after the method signature
    
    ---------
    
    Co-authored-by: Shahar Shwartz <shahar.shwartz@hibob.io>

---
+++ src/main/kotlin/com/hibob/academy/employeeFeedback/resource/FeedbackResource.kt
@@ -0,0 +1,94 @@
+package com.hibob.academy.employeeFeedback.resource
+
+import com.hibob.academy.employeeFeedback.model.*
+import com.hibob.academy.employeeFeedback.service.feedback.FeedbackCreator
+import com.hibob.academy.employeeFeedback.service.feedback.FeedbackFetcher
+import com.hibob.academy.employeeFeedback.service.feedback.FeedbackUpdator
+import com.hibob.academy.employeeFeedback.service.feedbackResponse.FeedbackResponseCreator
+import com.hibob.academy.employeeFeedback.service.feedbackResponse.FeedbackResponseFetcher
+import com.hibob.academy.employeeFeedback.service.feedbackResponse.FeedbackResponseUpdator
+import com.hibob.academy.employeeFeedback.utils.RequireRole
+import com.hibob.academy.filters.AuthenticationFilter
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.ws.rs.*
+import jakarta.ws.rs.core.MediaType
+import jakarta.ws.rs.core.Response
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestBody
+
+@Controller
+@Path("/api/feedback")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+class FeedbackResource(
+    private val feedbackCreator: FeedbackCreator,
+    private val feedbackFetcher: FeedbackFetcher,
+    private val feedbackUpdator: FeedbackUpdator,
+    private val feedbackResponseCreator: FeedbackResponseCreator,
+    private val feedbackResponseFetcher: FeedbackResponseFetcher,
+    private val feedbackResponseUpdator: FeedbackResponseUpdator,
+    private val request: HttpServletRequest
+
+) {
+
+    private fun getLoggedInUser(): LoggedInUser {
+        val userId = request.getAttribute(AuthenticationFilter.USER_ID) as Long
+        val companyId = request.getAttribute(AuthenticationFilter.COMPANY_ID) as Long
+        return LoggedInUser(userId, companyId)
+    }
+
+    @POST
+    @Path("/submit")
+    @RequireRole(Role.EMPLOYEE, Role.HR, Role.ADMIN)
+    fun submitFeedback(@RequestBody createFeedbackRequest: CreateFeedbackRequest) : Response {
+        val loggedInUser = getLoggedInUser()
+        val feedbackId = feedbackCreator.createFeedback(loggedInUser, createFeedbackRequest)
+        return Response.status(Response.Status.CREATED).entity(feedbackId).build()
+    }
+
+    @POST
+    @Path("/view")
+    @RequireRole(Role.HR, Role.ADMIN)
+    fun viewFeedback(@RequestBody filters: FilterFeedbackRequest) : Response {
+        val companyId = getLoggedInUser().companyId
+        val feedbacks = feedbackFetcher.getFeedbacksByCompany(companyId, filters)
+        return Response.status(Response.Status.OK).entity(feedbacks).build()
+    }
+
+    @POST
+    @Path("/{feedback_id}/response")
+    @RequireRole(Role.HR)
+    fun submitFeedbackResponse(@PathParam("feedback_id") feedbackId: Long, @RequestBody createFeedbackResponseRequest: CreateResponseRequest) : Response {
+        val loggedInUser = getLoggedInUser()
+        val responseId = feedbackResponseCreator.createFeedbackResponse(loggedInUser, feedbackId, createFeedbackResponseRequest)
+        return Response.status(Response.Status.CREATED).entity(responseId).build()
+    }
+
+    @GET
+    @Path("/{feedback_id}/status")
+    @RequireRole(Role.EMPLOYEE, Role.HR, Role.ADMIN)
+    fun getFeedbackStatus(@PathParam("feedback_id") feedbackId: Long) : Response {
+        val companyId = getLoggedInUser().companyId
+        val status = feedbackFetcher.getFeedbackStatusById(companyId, feedbackId)
+        return Response.status(Response.Status.OK).entity(status).build()
+    }
+
+    @PUT
+    @Path("/{feedback_id}/status")
+    @RequireRole(Role.HR)
+    fun updateFeedbackStatus(@PathParam("feedback_id") feedbackId: Long, @RequestBody updateFeedbackStatusRequest: UpdateFeedbackStatusRequest) : Response {
+        val loggedInUser = getLoggedInUser()
+        feedbackUpdator.updateFeedbackStatus(loggedInUser, feedbackId, updateFeedbackStatusRequest)
+        return Response.status(Response.Status.OK).build()
+    }
+
+    @PUT
+    @Path("/{feedback_id}/response")
+    @RequireRole(Role.HR)
+    fun updateFeedbackResponse(@PathParam("feedback_id") feedbackId: Long, @RequestBody updateFeedbackResponseRequest: UpdateFeedbackResponseRequest) : Response {
+        val loggedInUser = getLoggedInUser()
+        feedbackResponseUpdator.updateFeedbackResponse(loggedInUser, feedbackId, updateFeedbackResponseRequest)
+        return Response.status(Response.Status.OK).build()
+    }
+}
+
