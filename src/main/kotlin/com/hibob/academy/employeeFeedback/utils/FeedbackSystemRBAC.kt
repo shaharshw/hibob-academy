a583e6907c27448a600f46c22aadb08011a9fcac
Shahar Shwartz <shahar.shwartz@hibob.io>
2024-09-25 10:11:07 +0300

    Finish rbac auth.

--- src/main/kotlin/com/hibob/academy/employeeFeedback/utils/FeedbackSystemRBAC.kt
+++ src/main/kotlin/com/hibob/academy/employeeFeedback/utils/FeedbackSystemRBAC.kt
@@ -1,7 +1,9 @@
 package com.hibob.academy.employeeFeedback.utils
 
 import com.hibob.academy.employeeFeedback.model.Role
+import com.hibob.academy.filters.AuthenticationFilter
 import com.hibob.academy.service.SessionService
+import jakarta.servlet.http.HttpServletRequest
 import org.aspectj.lang.annotation.Aspect
 import org.aspectj.lang.annotation.Before
 import org.springframework.http.HttpStatus
@@ -14,15 +16,21 @@ annotation class RequireRole(vararg val roles: Role)
 
 @Aspect
 @Component
-class RoleCheckAspect(private val sessionService: SessionService) {
+class RoleCheckAspect(
+    private val sessionService: SessionService,
+    private val request: HttpServletRequest
+) {
 
     @Before("@annotation(requireRole)")
     fun checkRole(requireRole: RequireRole) {
-        val token = "token" // TODO: get token from request
-        val currentUserRole = sessionService.getCurrentUserRole(token)
+        val token = request.getAttribute(AuthenticationFilter.TOKEN_ATTRIBUTE) as? String
+            ?: throw ResponseStatusException(HttpStatus.UNAUTHORIZED, "Missing or invalid token")
+
+        val currentUserRole = sessionService.getCurrentUserRoleFromToken(token)
 
         if (!requireRole.roles.contains(currentUserRole)) {
             throw ResponseStatusException(HttpStatus.FORBIDDEN, "Access denied for role: $currentUserRole")
         }
     }
+
 }
