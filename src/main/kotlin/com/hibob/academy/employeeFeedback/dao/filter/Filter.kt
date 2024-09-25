98bc533a43a646bdda899008263362392e4121aa
Shahar Shwartz <shahar.shwartz@hibob.io>
2024-09-25 14:29:05 +0300

    Adding uppercase.

--- src/main/kotlin/com/hibob/academy/employeeFeedback/dao/filter/Filter.kt
+++ src/main/kotlin/com/hibob/academy/employeeFeedback/dao/filter/Filter.kt
@@ -5,6 +5,7 @@ import com.hibob.academy.employeeFeedback.dao.table.FeedbackTable
 import com.hibob.academy.employeeFeedback.model.Department
 import com.hibob.academy.employeeFeedback.model.FeedbackStatus
 import org.jooq.Condition
+import org.jooq.impl.DSL.upper
 import java.sql.Date
 import java.time.LocalDate
 
@@ -14,7 +15,9 @@ interface Filter<T> {
 
 class DepartmentFilter(private val department: Department?) : Filter<EmployeeTable> {
     override fun apply(table: EmployeeTable): Condition? {
-        return department?.name?.let { table.department.eq(it) }
+        return department?.name?.let {
+            upper(table.department).eq(it.uppercase()) // Use upper() for case-insensitive comparison
+        }
     }
 }
 
@@ -26,7 +29,9 @@ class DateFilter(private val date: LocalDate?) : Filter<FeedbackTable> {
 
 class StatusFilter(private val status: FeedbackStatus?) : Filter<FeedbackTable> {
     override fun apply(table: FeedbackTable): Condition? {
-        return status?.name?.let { table.status.eq(it) }
+        return status?.name?.let {
+            upper(table.status).eq(it.uppercase()) // Use upper() for case-insensitive comparison
+        }
     }
 }
 
