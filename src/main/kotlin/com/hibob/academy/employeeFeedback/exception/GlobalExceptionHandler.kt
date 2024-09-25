5484263fc759aaf9188e0eee9b1219e9d7b80b28
Shahar Shwartz <shahar.shwartz@hibob.io>
2024-09-25 11:59:26 +0300

    fix GlobalExceptionHandler

--- src/main/kotlin/com/hibob/academy/employeeFeedback/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/hibob/academy/employeeFeedback/exception/GlobalExceptionHandler.kt
@@ -1,5 +1,6 @@
 package com.hibob.academy.employeeFeedback.exception
 
+import jakarta.ws.rs.core.Response
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException::class)
-    fun handleIllegalArgumentException(ex: IllegalArgumentException): ResponseEntity<String> {
-        return ResponseEntity(ex.message, HttpStatus.BAD_REQUEST)
+    fun handleIllegalArgumentException(ex: IllegalArgumentException): Response {
+        return Response.status(Response.Status.BAD_REQUEST).entity(ex.message).build()
     }
 }
